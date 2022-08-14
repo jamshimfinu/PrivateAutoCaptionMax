e9be35ce011b2577b1cb969085e1177860592f17
jamshimfinu <111169920+jamshimfinu@users.noreply.github.com>
2022-08-14 23:28:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     MT_BOT_TOKEN = os.environ.get("MT_BOT_TOKEN", "")
     API_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
-    CAPTION = os.environ.get("CAPTION", "movieCloudAI @Moviebell01)
+    CAPTION = os.environ.get("CAPTION", "@movieCloudAI @Moviebell01")
     BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
     URL_LINK = os.environ.get("LINK", "https://t.me/movieCloudAI")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
